42508646b9748c5b42df89a56149db7fd7df043e
Vikram Singh <58028680+bcalm@users.noreply.github.com>
2020-06-25 18:17:53 +0530

    Added ModelStateMentParser class. It will covert data to sqlite string form.

--- src/models.js
+++ src/models.js
@@ -1,1 +1,39 @@
+class ModelStatementParser {
+  constructor(schema, tableName) {
+    this.tableName = tableName;
+    this.schema = schema;
+  }
 
+  createTable() {
+    const columnData = this.schema.join(',');
+    return `CREATE TABLE ${this.tableName}(${columnData});`;
+  }
+
+  insert({columns, values}) {
+    const insertedData = values.map((statement) => `(${statement})`).join();
+    return `INSERT INTO ${this.tableName} (${columns})
+                  VALUES ${insertedData};`;
+  }
+
+  update({columns, where}) {
+    const dataToUpdate = columns.join(',');
+    return `UPDATE ${this.tableName}
+              SET ${dataToUpdate}
+              WHERE ${where};`;
+  }
+
+  delete(conditions) {
+    return `DELETE FROM ${this.tableName} 
+              WHERE ${conditions.join('OR')};`;
+  }
+
+  select({columns, where, orderBy, groupBy}) {
+    let query = `SELECT ${columns} FROM ${this.tableName}`;
+    query = where ? query + ` WHERE ${where}` : query;
+    query = groupBy ? query + ` GROUP BY ${groupBy}` : query;
+    query = orderBy ? query + ` ORDER BY ${orderBy}` : query;
+    return query;
+  }
+}
+
+module.exports = {ModelStatementParser};
