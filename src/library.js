6d6dcefcd9b65353c6c93b3ddcc71d82a1f4326e
Vikram Singh <58028680+bcalm@users.noreply.github.com>
2020-06-26 08:46:28 +0530

    Added methods in Library class.

--- src/library.js
+++ src/library.js
@@ -67,27 +67,81 @@ class Library {
     // this.libraryLog
   }
 
-  addBook(bookDetail, bookCopiesDetail) {
-    // this.booksTitles
-    // this.bookCopies
+  addBook(bookTitles, bookCopies, bookDetail, bookCopiesDetail) {
+    const insertionDetail = {
+      columns: bookTitles,
+      values: bookDetail,
+    };
+    const sql1 = this.book_titles.insert(insertionDetail);
+    const insertionDetail = {
+      columns: bookCopies,
+      values: bookCopiesDetail,
+    };
+    const sql1 = this.book_copies.insert(insertionDetail);
+    return (
+      runSql(sql1, [], this.db.all.bind(this.db)) && runSql(sql2, [], this.db.all.bind(this.db))
+    );
   }
 
-  addCopy(ISBN, copyDetail) {
-    // this.booksTitles
-    // this.bookCopies
+  addCopy(ISBN, bookDetail, copyDetail) {
+    const updationDetail = {
+      columns: ['no_of_copies_total=no_of_copies +1'],
+      where: [`ISBN=${ISBN}`],
+    };
+    const updateSQL = this.book_titles.update(updationDetail);
+    const insertionDetail = {
+      columns: bookDetail,
+      values: copyDetail,
+    };
+    const sql = this.book_copies.insert(insertionDetail);
+    return (
+      runSql(sql, [], this.db.all.bind(this.db)) && runSql(updateSQL, [], this.db.all.bind(this.db))
+    );
   }
 
   popularBooks() {
-    //this.libraryLog
+    const queryDetails = {
+      columns: ['serial_number', 'count(serial_number) occurring_time', 'serial_number'],
+      where: "action = 'issue' AND date_of_action BETWEEN date('now', '-1year') AND date('now')",
+      groupBy: 'serial_number',
+    };
+    const queryDetails2 = {
+      columns: ['occurring_time', ' tab1.serial_number', 'tab2.ISBN', 'tab3.title'],
+    };
+
+    const subSql = this.library_log.select(queryDetails).slice(0, -1);
+    const subSql2 = this.book_titles
+      .select(queryDetails2)
+      .replace(/book_titles/, 'issuedBook tab1');
+
+    const sql = ` WITH issued_books as (
+                  ${subSql})
+                  ${subSql2}
+                  Left join ${this.book_copies} tab2
+                  ON tab1.serial_number = tab2.serial_number
+
+                  LEFT join ${this.book_titles} tab3
+                  ON tab2.ISBN = tab3.ISBN
+                  order by occurring_time DESC;`;
+    return runSql(sql, [], this.db.all.bind(this.db));
   }
 
   regularUsers() {
-    //this.libraryLog
+    const queryDetails = {
+      columns: ['max(library_user_id)'],
+    };
+    const sql = this.library_log.select(queryDetails);
+    return runSql(sql, [], this.db.all.bind(this.db));
   }
 
   defaulterUsers() {
-    //this.libraryLog
+    const queryDetails = {
+      columns: ['issued_date', 'library_user_id', 'serial_number'],
+      where: "is_available = false AND issued_date < date('now', '-10 day')",
+    };
+    const sql = this.book_copies.select(queryDetails);
+    return runSql(sql, [], this.db.all.bind(this.db));
   }
 }
 
-module.exports = { Library };
+module.exports = {Library};
